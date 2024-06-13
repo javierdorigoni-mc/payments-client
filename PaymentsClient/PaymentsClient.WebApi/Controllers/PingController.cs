7d268c4f45a0c6c9c26d2ce3c8930a22df46d11a
Javier.Dorigoni <javier.dorigoni@mastercard.com>
2024-06-13 12:28:15 +0200

    Add Web Api project

---
+++ PaymentsClient/PaymentsClient.WebApi/Controllers/PingController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentsClient.WebApi.Controllers;
+
+[ApiController]
+[Route("api")]
+public class PingController : ControllerBase
+{
+    private readonly ILogger<PingController> _logger;
+
+    public PingController(ILogger<PingController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "ping")]
+    public IActionResult Ping()
+    {
+        _logger.LogInformation("Ping called");
+        return Ok("Pong");
+    }
+}
